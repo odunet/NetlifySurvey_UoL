b80467c7db7da6882f9ebafa5377ca5b993a28e5
Ayo <odunet2000@yahoo.com>
2021-03-07 21:28:23 +0100

    Enable form data storage ing JSON format using JSONBIN.io

--- ReadMe.md
+++ ReadMe.md
@@ -2,13 +2,13 @@
 
 This is a sample survey form with advanced CSS (CSS-Flex, CSS-Grid, and CSS-Animation). This is deployment ready for Netlify
 
-#### Backend
+### Backend
 
-Enabled by Netlify
+* Form Submission Enabled by Netlify
+* JSON Form data storage by [JSONBIN.io](https://jsonbin.io/)
 
 
-License
-----
+### License
 
 MIT
 **Free**
--- javascript/script.js
+++ javascript/script.js
@@ -9,22 +9,71 @@ var radio7 = document.getElementById("seventh");
 var radio8 = document.getElementById("eight");
 var radio9 = document.getElementById("ninth");
 
+//Initialize name of survey participant
+var surveyor = document.getElementById("surveyor");
+
 //Initialize table body
 var tableBody = document.getElementById("tableBody");
 
-//Pulls data from the DOM.
-// for (const property in radio1.children) {
-//         if (property != 0 && typeof(radio1.children[property]) != "function" && property != "length") { 
-//         console.log(radio1.children[property].childNodes[0].checked);       
-//     }
-// }
+//Initialize survey submit button
+var surveySubmit = document.getElementById("surveySubmit");
 
-tableBody.addEventListener("click", (e) => {
-    if (e.target.tagName == "INPUT") {
-        e.target.checked != e.target.checked;
+//Initialize empty list to take data
+var radioResult = [];
 
-        let clickedRadioId = e.target.id;
-        let clickedRadioRow = Number(clickedRadioId.slice(0,1)) - 1;
-        let clickedRadioColumn = Number(clickedRadioId.slice(2));    
+//Add the header
+radioResult.push(["Question", "V-Likely", "Likely", "Nueral", "Unlikely", "V-Unlikely"]);
+
+//On click of a button, it takes the table and destructures in to rows
+surveySubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    for (const property in tableBody.children) {
+        if (typeof(tableBody.children[property]) != "function" && property != "length") { 
+            var tableRow = tableBody.children[property];
+
+            //Ater destructuring the tables into rowns, it destructures each row into the individual objects,
+            //further selecting each radio button and checking if it is clicked. Final result is saved into 
+            //a 2X2 array
+            let tempArray = [];
+            for (const innerP in tableRow.children) {
+                if (innerP != 0 && innerP != "length" && typeof(tableRow.children[innerP]) != "function") {
+                    tempArray.push(tableRow.children[innerP].children[0].checked);    
+                } 
+                //This will store the question which has object key 0, in the array
+                if (innerP == 0) {
+                    tempArray.push([(tableRow.children[innerP].innerHTML).toString()]);
+                }
+            };
+            radioResult.push(tempArray);  
+        };
     };
+
+    // POST request using fetch() 
+    fetch("https://api.jsonbin.io/v3/b", { 
+    
+        // Adding method type 
+        method: "POST", 
+        
+        // Adding body or contents to send 
+        body: JSON.stringify({ 
+            data: "Ayokunle", 
+            email: radioResult, 
+        }), 
+        
+        // Adding headers to the request 
+        headers: { 
+            "Content-type": "application/json",
+            "X-Master-Key": "$2b$10$QFZzyMSPBRMT8aYlYcqfdO0v/Pu3/rqH9.XV0R2pGiyFvtCK1Pc2G",
+            "X-BIN-NAME": `${surveyor.value}`,
+            "X-COLLECTION-ID": "60452abe5e29de07fced5945",
+        }
+    }) 
+
+    // Converting to JSON 
+    .then(response => response.json()) 
+
+    // Displaying results to console 
+    .then(json => {
+        console.log(json)
+    }); 
 });
